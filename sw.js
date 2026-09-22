1117729681b338ea2752b117ae64ae0010ba3bc1
Muhammad RezaRamadhani <mhdrezaramadhani@gmail.com>
2026-09-22 18:33:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fuel-pjp-v1';
+const CACHE_NAME = 'refuel-pjp-v2';
 const urlsToCache = [ './', './index.html', './manifest.json', './icon-192.png', './icon-512.png' ];
 
 self.addEventListener('install', event => {
